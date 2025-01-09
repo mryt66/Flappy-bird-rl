8ec37c57d5f8fcadcb2f58988c25634ec0ca40aa
Konrad <55025128+KonraW@users.noreply.github.com>
2025-01-09 15:00:21 +0100

    rep

--- visualizationUnity.py
+++ visualizationUnity.py
@@ -45,7 +45,7 @@ def write_action(action):
         with open(temp_file, "w") as file:
             json.dump({"action": int(action)}, file)
         os.replace(temp_file, ACTION_FILE)
-        # print(f"Action: {action}")
+        print(f"Action: {action}")
         os.remove(ISDONE_FILE)
     except Exception as e:
         # print(f"Error writing action: {e}")
@@ -72,14 +72,7 @@ def main():
 
     print("Python agent for visualization started. Waiting for Unity...")
 
-    episode=4478
-    epsilon_global=0.9985**4478
-    print(f"Starting episode: {episode}, Epsilon: {epsilon_global:.4f}")
-    
-    best_agent_epsilon=0.1 #!!!!!!!!
-    best_agent_reward=0
-    best_agent_score=0
-    total_reward=0
+
     while True:
         if not os.path.exists(ISDONE_FILE):
             # print("Czekam na plik isdone.json...")
@@ -87,24 +80,10 @@ def main():
         else:
             state, reward, score = read_state()
             if state is not None:
-                # print(f"State: {state}")
+                print(f"State: {state}")
                 action = agent.act(state)
                 write_action(action)
-                # print(reward)
-                total_reward += reward
-               
-        if reward==-10:
-            episode+=1
-            best_agent_reward = total_reward
-            best_agent_score = score
-            total_reward=0
-            epsilon_global*=0.9985
-            epsilon_global=0.01
-            print()
-            print(
-                f"Episode {episode}, Reward: {best_agent_reward:.2f}, Score: {best_agent_score}, Epsilon: {epsilon_global:.4f}"
-            )
-
+              
 
 if __name__ == "__main__":
     main()
